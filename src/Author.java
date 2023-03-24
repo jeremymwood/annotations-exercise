d814008c35288510ffadbbd54f5d28097e82bc5c
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-24 10:16:03 -0500

    1016

---
+++ src/Author.java
@@ -0,0 +1,40 @@
+/**
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+@SuppressWarnings("unchecked")
+
+public class Author extends Person {
+
+    private List books;
+
+    public Author(String firstName, String lastName) {
+        super(firstName, lastName);
+        books = new ArrayList();
+    }
+
+    /**
+     * @deprecated Use publishedBooks instead
+     */
+    @Deprecated
+    public List<String> getBooks() {
+        return books;
+    }
+
+    public List<String> publishedBooks() {
+        return books;
+    }
+
+    public void addBook(String book) {
+        books.add(book);
+    }
+
+//    @Override
+//    @SuppressWarnings("unchecked")
+    public String sortName() {
+
+        return String.format("%s, %s", lastName, firstName);
+    }
+}
